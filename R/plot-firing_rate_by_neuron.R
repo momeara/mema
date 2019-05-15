9f4abbc9abd6efa6af2299cef1fd9bcfcec1cdfd
Matthew O'Meara <mattjomeara@gmail.com>
2019-05-15 16:05:36 -0700

    Now it at least builds

--- R/plot-firing_rate_by_neuron.R
+++ R/plot-firing_rate_by_neuron.R
@@ -1,75 +1,69 @@
 # -*- tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
 # vi: set ts=2 noet:
 
-library(plyr)
-library(dplyr)
-library(scales)
-library(ggplot2)
-
-
-#' Firing rate by neuron 
+#' Firing rate by neuron
 #'
 #'  histogram of average firing rate
 #'     x-axis firing rate on the log-scale
 #'     y-axis number of neurons in the histogram bin
-#'  
+#'
 #'  if there is spread in the average firing rate then this variation should be
 #'  accounted for in a model of effects of the treatment on the average firing rate
-#'   
+#'
 #' firing:
 #'   data.frame with columns [neuron_index, timestamp, treatment]
-#' 
+#'
 #' experiment_tag:
 #'   identifier for the experiment used in the figure subtitle and output filename
-#'   
+#'
 #' plot_width/plot_height:
-#'   dimensions of the output plot  
-#'   
+#'   dimensions of the output plot
+#'
 #' returns:
 #'   the ggplot2
 #'   Saves the result to product/figures/firing_rate_by_neuron_<experiment_tag>_<date_code>.(pdf|png)
 #'   It save both .pdf and .png because it's easier to email etc small pngs
 #'   while for use in an a manuscript having the vector version means that it can be tweaked with illustrator
-#'   
+#'
 #'@export
 firing_rate_by_neuron <- function(
-  firing,
-  experiment_tag,
-  plot_width=7,
-  plot_height=4,
-  output_base="product/figures",
-  verbose=TRUE){
-  
-  total_exposure <- firing %>%
-    dplyr::distinct(treatment) %>%
-    dplry::mutate(exposure = end[1] - begin[1]) %>%
-    dplyr::summarize(total_exposure=sum(exposure)) %>%
-    magrittr::extract2("total_exposure")
-  
-  data <- firing %>%
-    dplyr::group_by(neuron_index) %>%
-    dplyr::summarize(mean_firing_rate = n() / total_exposure)
-  
-  ggplot2::ggplot(data=data) +
-    ggplot2::theme_bw() +
-    ggplot2::geom_histogram(
-      aes(x=log(mean_firing_rate)),
-      bins=30) +
-    ggplot2::ggtitle("Per-neuron firing rate", subtitle=experiment_tag) +
-    ggplot2::scale_x_continuous(
-      "Log(Firing / second)")
-  
-  pdf_path <- paste0(output_base, "/firing_rate_by_neuron_", experiment_tag, "_", date_code(), ".pdf")
-  if(verbose){
-    cat("Saving firing_rate_by_neuron  plot for experiment '", experiment_tag, "' to '", pdf_path, "'\n", sep="")
-  }
-  ggplot2::ggsave(pdf_path, width=10, height=10)
-  
-  png_path <- paste0(output_base, "/firing_rate_by_neuron_", experiment_tag, "_", date_code(), ".png")
-  if(verbose){
-    cat("Saving firing_rate_by_neuron plot for experiment '", experiment_tag, "' to '", png_path, "'\n", sep="")
-  }
-  ggplot2::ggsave(png_path, width=plot_width, height=plot_height)
-  
-  p
+	firing,
+	experiment_tag,
+	plot_width=7,
+	plot_height=4,
+	output_base="product/figures",
+	verbose=TRUE){
+
+	total_exposure <- firing %>%
+		dplyr::distinct(treatment) %>%
+		dplry::mutate(exposure = end[1] - begin[1]) %>%
+		dplyr::summarize(total_exposure=sum(exposure)) %>%
+		magrittr::extract2("total_exposure")
+
+	data <- firing %>%
+		dplyr::group_by(neuron_index) %>%
+		dplyr::summarize(mean_firing_rate = n() / total_exposure)
+
+	ggplot2::ggplot(data=data) +
+		ggplot2::theme_bw() +
+		ggplot2::geom_histogram(
+			aes(x=log(mean_firing_rate)),
+			bins=30) +
+		ggplot2::ggtitle("Per-neuron firing rate", subtitle=experiment_tag) +
+		ggplot2::scale_x_continuous(
+			"Log(Firing / second)")
+
+	pdf_path <- paste0(output_base, "/firing_rate_by_neuron_", experiment_tag, "_", date_code(), ".pdf")
+	if(verbose){
+		cat("Saving firing_rate_by_neuron  plot for experiment '", experiment_tag, "' to '", pdf_path, "'\n", sep="")
+	}
+	ggplot2::ggsave(pdf_path, width=10, height=10)
+
+	png_path <- paste0(output_base, "/firing_rate_by_neuron_", experiment_tag, "_", date_code(), ".png")
+	if(verbose){
+		cat("Saving firing_rate_by_neuron plot for experiment '", experiment_tag, "' to '", png_path, "'\n", sep="")
+	}
+	ggplot2::ggsave(png_path, width=plot_width, height=plot_height)
+
+	p
 }
--- R/plot-waveform_correlation_matrix.R
+++ R/plot-waveform_correlation_matrix.R
@@ -1,78 +1,68 @@
 # -*- tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
 # vi: set ts=2 noet:
 
-library(plyr)
-library(dplyr)
-library(reshape2)
-library(seriation)
-library(viridis)
-library(gplots)
-
-
 #' plot Correlation matrix of waveforms
 #'
 #'   Plot a correlation matrix as a heatmap between all waveforms in the experiment
-#' 
+#'
 #' waveform:
 #'   data.frame with columns [neuron_index, timestamp, voltage]
-#' 
+#'
 #' experiment_tag:
 #'   identifier for the experiment used in the figure subtitle and output filename
-#'   
+#'
 #' plot_width/plot_height:
-#'   dimensions of the output plot  
-#'   
+#'   dimensions of the output plot
+#'
 #' returns:
 #'   the ggplot2
 #'   Saves the result to product/figures/waveform_correlation_matrix_<experiment_tag>_<date_code>.(pdf|png)
 #'   It save both .pdf and .png because it's easier to email etc small pngs
 #'   while for use in an a manuscript having the vector version means that it can be tweaked with illustrator
-#'   
+#'
 #'@export
 waveform_correlation_matrix <- function(
-  waveform,
-  experiment_tag,
-  plot_width=10,
-  plot_height=10,
-  output_base="product/figures",
-  verbose=TRUE){
-  
+	waveform,
+	experiment_tag,
+	plot_width=10,
+	plot_height=10,
+	output_base="product/figures",
+	verbose=TRUE){
 
-  correlations <- waveform %>%
-    reshape2::acast(time_step ~ neuron_index, value.var="voltage") %>%
-    cor()
-  
-  d <- dist(correlations)
-  o_row <- seriate(d, method="OLO", control=NULL)[[1]]
-  args=list(
-    trace="none",
-    density.info="none",
-    col=viridis::viridis(100),
-    cexRow=1,
-    cexCol=1,
-    dendrogram="none",
-    key=FALSE,
-    keysize=0.03,
-    x=correlations,
-    Colv=as.dendrogram(o_row),
-    Rowv=as.dendrogram(o_row))
-  
-  pdf_path <- paste0(output_base, "/waveform_correlation_matrix_", experiment_tag, "_", date_code(), ".pdf")
-  if(verbose){
-    cat("Saving waveform_correlation_matrix plot for experiment '", experiment_tag, "' to '", pdf_path, "'\n", sep="")
-  }
-  pdf("product/demo_waveform_correlation_matrix_190513.pdf", heigh=6, width=6)
-  do.call(gplots::heatmap.2,args=args)
-  dev.off()
-  
-  png_path <- paste0(output_base, "/waveform_correlation_matrix_", experiment_tag, "_", date_code(), ".png")
-  if(verbose){
-    cat("Saving waveform_correlation_matrix plot for experiment '", experiment_tag, "' to '", pdf_png, "'\n", sep="")
-  }
-  png(png_path, units="in", heigh=plot_height, width=plot_width)
-  do.call(gplots::heatmap.2,args=args)
-  dev.off()
-  
+
+	correlations <- waveform %>%
+		reshape2::acast(time_step ~ neuron_index, value.var="voltage") %>%
+		cor()
+
+	d <- dist(correlations)
+	o_row <- seriate(d, method="OLO", control=NULL)[[1]]
+	args=list(
+		trace="none",
+		density.info="none",
+		col=viridis::viridis(100),
+		cexRow=1,
+		cexCol=1,
+		dendrogram="none",
+		key=FALSE,
+		keysize=0.03,
+		x=correlations,
+		Colv=as.dendrogram(o_row),
+		Rowv=as.dendrogram(o_row))
+
+	pdf_path <- paste0(output_base, "/waveform_correlation_matrix_", experiment_tag, "_", date_code(), ".pdf")
+	if(verbose){
+		cat("Saving waveform_correlation_matrix plot for experiment '", experiment_tag, "' to '", pdf_path, "'\n", sep="")
+	}
+	pdf("product/demo_waveform_correlation_matrix_190513.pdf", heigh=6, width=6)
+	do.call(gplots::heatmap.2,args=args)
+	dev.off()
+
+	png_path <- paste0(output_base, "/waveform_correlation_matrix_", experiment_tag, "_", date_code(), ".png")
+	if(verbose){
+		cat("Saving waveform_correlation_matrix plot for experiment '", experiment_tag, "' to '", pdf_png, "'\n", sep="")
+	}
+	png(png_path, units="in", heigh=plot_height, width=plot_width)
+	do.call(gplots::heatmap.2,args=args)
+	dev.off()
+
 }
-  
-  dev.off()
