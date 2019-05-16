dd19c8faf4a606c01a88d6cfb3ba24bae5ef580d
Matthew O'Meara <mattjomeara@gmail.com>
2019-05-16 16:07:18 -0700

    revamp plot_firing_density_by_neuron

--- R/model_treatment_log_poisson.R
+++ R/model_treatment_log_poisson.R
@@ -31,58 +31,58 @@ library(rstan)
 #'
 #'@export
 model_treatment_log_poisson <- function(
-  experiment,
-  output_base="intermediate_data/models",
-  verbose=TRUE,
-  ...){
+	experiment,
+	output_base="intermediate_data/models",
+	verbose=TRUE,
+	...){
 
-  exposure_counts <- experiment$firing %>%
-    dplyr::group_by(neuron_index, treatment) %>%
-    dplyr::summarize(
-      count = dplyr::n(),
-      exposure = end[1]-begin[1]) %>%
-    dplyr::ungroup()
-  
-  
-  if(verbose){
-    cat("fitting log-poisson model for firing counts for experiment '", experiment$tag, "'\n", sep="")
-  }
-  
-  if(!is.na(output_base)){
-    model_path <- paste0(output_base, "/model_log_poisson_", experiment$tag, ".stan")
-    cat("Saving stan model to '", model_path, "'", sep="")
-  } else {
-    model_path <- NULL
-  }
-  
-  fit_log_poisson <- brms::brm(
-    data=exposure_counts,
-    formula=count ~ stats::offset(log(exposure)) + treatment + (1|neuron_index),
-    family=stats::poisson(link="log"),
-    save_model=model_path,
-    ...)
+	exposure_counts <- experiment$firing %>%
+		dplyr::group_by(neuron_index, treatment) %>%
+		dplyr::summarize(
+			count = dplyr::n(),
+			exposure = end[1]-begin[1]) %>%
+		dplyr::ungroup()
 
-  if(!is.null(output_base)){
-    if(!dir.exists(output_base)){
-      if(verbose){
-        cat("creating output directory '", output_base, "'\n", sep="")
-      }
-      dir.create(output_base, showWarnings = FALSE)
-    }
-    
-    fit_path <- paste0(output_base, "/model_log_poisson_fit_", experiment$tag, ".Rdata")
-    if(verbose){
-      cat("Saving log-poisson model fit to '", fit_path, "'\n", sep="")
-    }
-    save(fit_log_poisson, file=fit_path)
-    
-    summary_path <- paste0(output_base, "/model_log_poisson_summary_", experiment$tag, ".txt")
-    if(verbose){
-      cat("Saving log-poisson model summary to '", summary_path)
-    }
-    summary(fit_log_poisson) %>%
-      capture.output(file=summary_path)
-  }
 
-  invisible(fit_log_poisson)
+	if(verbose){
+		cat("fitting log-poisson model for firing counts for experiment '", experiment$tag, "'\n", sep="")
+	}
+
+	if(!is.na(output_base)){
+		model_path <- paste0(output_base, "/model_log_poisson_", experiment$tag, ".stan")
+		cat("Saving stan model to '", model_path, "'", sep="")
+	} else {
+		model_path <- NULL
+	}
+
+	fit_log_poisson <- brms::brm(
+		data=exposure_counts,
+		formula=count ~ stats::offset(log(exposure)) + treatment + (1|neuron_index),
+		family=stats::poisson(link="log"),
+		save_model=model_path,
+		...)
+
+	if(!is.null(output_base)){
+		if(!dir.exists(output_base)){
+			if(verbose){
+				cat("creating output directory '", output_base, "'\n", sep="")
+			}
+			dir.create(output_base, showWarnings = FALSE)
+		}
+
+		fit_path <- paste0(output_base, "/model_log_poisson_fit_", experiment$tag, ".Rdata")
+		if(verbose){
+			cat("Saving log-poisson model fit to '", fit_path, "'\n", sep="")
+		}
+		save(fit_log_poisson, file=fit_path)
+
+		summary_path <- paste0(output_base, "/model_log_poisson_summary_", experiment$tag, ".txt")
+		if(verbose){
+			cat("Saving log-poisson model summary to '", summary_path)
+		}
+		summary(fit_log_poisson) %>%
+			capture.output(file=summary_path)
+	}
+
+	invisible(fit_log_poisson)
 }
---
+++ R/plot_firing_density_by_neuron.R
@@ -0,0 +1,99 @@
+# -*- tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+# vi: set ts=2 noet:
+
+#' Firing smoothed per-neuron firing density by neuron across the experiment
+#'
+#'  heat-map average firing rate
+#'     x-axis experiment in seconds
+#'     y-axis neuron index
+#'     cell KDE smoothe estimate of the firing rate
+#'
+#'
+#' experiment:
+#'   experiment dataset loaded with mema::load_experiment(...)
+#'
+#' plot_width/plot_height:
+#'   dimensions of the output plot
+#'
+#' returns:
+#'   the ggplot2
+#'   Saves the result to product/plots/firing_rate_<experiment_tag>_<date_code>.(pdf|png)
+#'   It save both .pdf and .png because it's easier to email etc small pngs
+#'   while for use in an a manuscript having the vector version means that it can be tweaked with illustrator
+#'
+#'@export
+plot_firing_density_by_neuron <- function(
+	experiment,
+	plot_width=7,
+	plot_height=4,
+	output_base="product/plots",
+	verbose=TRUE){
+
+
+  data <- experiment$firing %>%
+    dplyr::select(neuron_index, time_step) %>%
+    plyr::ddply("neuron_index", function(data){
+      density_estimate <- stats::density(
+        x=data$time_step,
+        from=experiment$treatments$begin %>% min,
+        to=experiment$treatments$end %>% max,
+        adjust=.01,
+        n=1000)
+      tibble::tibble(
+        time_step=density_estimate$x,
+        firing_density=density_estimate$y) %>%
+        dplyr::mutate(
+          normalized_log_firing_density=log(firing_density/max(firing_density)+1)
+        )
+    })
+  
+  p <- ggplot2::ggplot(data=data) +
+    ggplot2::theme_bw() +
+    ggplot2::geom_tile(
+      mapping=ggplot2::aes(
+        x=time_step,
+        y=neuron_index,
+        color=normalized_log_firing_density)) +
+    ggplot2::geom_rect(
+      data= experiment$treatments,
+      mapping=ggplot2::aes(xmin=begin, xmax=end, ymin=-Inf, ymax=Inf, group=treatment),
+      color="white", alpha=0) +
+    ggplot2::ggtitle("Per-neuron firing density across experiment", subtitle=experiment$tag) +
+    ggplot2::scale_x_continuous(
+      name="Seconds",
+      breaks=experiment$treatments$begin,
+      expand=c(0,0),
+      sec.axis = ggplot2::dup_axis(
+        name=NULL,
+        breaks=with(experiment$treatments, begin + (end - begin)/2),
+        labels=experiment$treatments$treatment)) +
+    ggplot2::scale_y_continuous(
+      name='Neuron Index',
+      expand=c(0,0)) +
+    ggplot2::scale_color_viridis_c("Per-neuron normalized log firing density") +
+    ggplot2::theme(legend.position="bottom") +
+    ggplot2::theme()
+
+	if(!is.null(output_base)){
+		if(!dir.exists(output_base)){
+			if(verbose){
+				cat("creating output directory '", output_base, "'\n", sep="")
+			}
+			dir.create(output_base, showWarnings = FALSE)
+		}
+
+		pdf_path <- paste0(output_base, "/firing_density_by_neuron_", experiment$tag, "_", date_code(), ".pdf")
+		if(verbose){
+			cat("Saving firing density by neuron plot for experiment '", experiment$tag, "' to '", pdf_path, "'\n", sep="")
+		}
+		ggplot2::ggsave(pdf_path, width=10, height=10)
+
+		png_path <- paste0(output_base, "/firing_density_by_neuron_", experiment$tag, "_", date_code(), ".png")
+		if(verbose){
+			cat("Saving firing density by neuron plot for experiment '", experiment$tag, "' to '", png_path, "'\n", sep="")
+		}
+		ggplot2::ggsave(png_path, width=plot_width, height=plot_height)
+	}
+
+	invisible(p)
+}
